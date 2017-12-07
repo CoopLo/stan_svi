47c7884fa664415e4b3bfc58455601589fdb2d84
CoopLo <lorsung2@illinois.edu>
2017-12-07 13:26:49 -0600

    worked on implementation

--- stan_trial.py
+++ stan_trial.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pickle
 from matplotlib import pyplot as plt
 from hashlib import md5
+import sys
 
 def StanModel_cache(model_code, model_name=None, **kwargs):
 	"""Use just as you would 'stan'"""
@@ -32,18 +33,34 @@ index = 100
 all_stock_data = pd.read_csv("../data/mquote201010.csv")
 # get data and put it into a dict because stan wants it that way 
 stock_data = np.ndarray.tolist(hf.stock_data_in_one_line(all_stock_data, index)[:data_points])
+stock_data = np.reshape(stock_data, (len(stock_data), 1))
 dict_data = dict({'data': stock_data})
 
-
 # comes from stan
-model_code = hf.get_model_code()
-model = StanModel_cache(model_code)
-results = model.vb(dict_data)
-sample_data = model.sampling(dict_data)
-model_data = sample_data.extract()['y']
+svi_code = hf.get_model_code()
 
-plt.subplot(211)
-plt.plot(stock_data)
-plt.subplot(212)
-plt.plot(model_data)
-plt.show()
+model = StanModel_cache(model_code = svi_code)
+
+model_data = {
+      'K': 5,
+      'N': stock_data.shape[0],
+      'T': 1,
+      'y': stock_data
+}
+
+results = model.vb(data = model_data,
+                   output_samples = 1000,
+                   iter = 4000,
+                   eval_elbo = 50,
+                   algorithm = 'meanfield',
+                   diagnostic_file = "~/Research/stan_svi")
+
+print(results.keys())
+#sample_data = model.sampling(dict_data)
+#model_data = sample_data.extract()['y']
+
+#plt.subplot(211)
+#plt.plot(stock_data)
+#plt.subplot(212)
+#plt.plot(model_data)
+#plt.show()
