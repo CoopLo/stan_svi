40233ad4cae34dc0706b7c20cc0a5e42c48c494d
CoopLo <lorsung2@illinois.edu>
2018-02-06 14:32:12 -0600

    Modifications to helper_funcs because thats where the stan model code is

--- new_helper_funcs.py
+++ new_helper_funcs.py
@@ -44,6 +44,21 @@ def create_dataset(dataset, look_back=1):
         dataY.append(dataset[i+look_back])
     return np.array(dataX), np.array(dataY)
 
+# create 3D data set where each vector is <(close-open)/open, (high-open)/open, (open-low)/open>
+# over n minute intervals. This simulates open, close, high and low for days.
+def create_3D_dataset(stock_data, interval):
+    new_data = []
+    for i in range(0, len(stock_data), interval):
+        inter_open = stock_data[i]
+        inter_close = stock_data[i+interval-1]
+        low = min(stock_data[i:i+interval-1])
+        high = max(stock_data[i:i+interval-1])
+
+        new_data.append(((inter_close-inter_open)/inter_open, (high-inter_open)/inter_open,
+                         (inter_open - low)/inter_open))
+
+    return np.asarray(new_data)
+
 def get_model_code():
 	return """
 	data {
@@ -51,86 +66,36 @@ def get_model_code():
 		int<lower=1> N; // number of data points
 		int<lower=1> T; // length of timeseries
 		real y[N,T]; // observations
+        real trans_mat[K,K]; // transition matrix
+        real p[N]; // initial distribution
+        real emit_mat[K,T]; // emission matrix
 	}
 	parameters {
-		real<lower=0,upper=1> p0 ;     //initial prob grp 1
-		real<lower=0,upper=1> TP[K] ;  //transition probs of staying in group
-		real mu[K]; // locations of mixture components
-		real<lower=0.0000001> sigma[K]; // scales of mixture components
-	}
-	transformed parameters {
-		//smoother estimate probability of group membership
-		//real<lower=0,upper=1> prob_grp[N,T];  
-		//one-step filter prediction of probabililty of group membership
-		real<lower=0,upper=1> pred[N,T];   
-	{
-	real F[N,T];   //filter forwards group membership prob
-  	real B1[N,T];  //backwards information filter from grp 1
-  	real B2[N,T];  //backwards information filter from grp 2
-  	real Z1[N,T];  //intermediate data
-  	real Z2[N,T];  //intermediate data
-  	real like1;    
-  	real like2;
-  	real p1;
-  	real p2;
-  	real k;
+
   	int i;
 
   	//Forwards algorithm
-  	for (n in 1:N) { F[n,1] = p0; 
-                   pred[n,1] = F[n,1];}
+  	for (n in 1:N) { F[n,1] = p[i]*emit_mat[n,1];}
+                   
   	for (t in 1:T){
     	for (n in 1:N) {
-      	//update prior using data
-      	like1 = exp(normal_lpdf(y[n,t]|mu[1],sigma[1]));
-      	like2 = exp(normal_lpdf(y[n,t]|mu[2],sigma[2]));
-      	p1 = F[n,t]*like1;
-      	p2 = (1-F[n,t])*like2;
-      	F[n,t] = p1/(p1+p2);
-
-      	//predict forward one timestep
-      	if (t != T) {
-        		p1 = F[n,t]*TP[1]+(1-F[n,t])*(1-TP[2]);
-        		p2 = F[n,t]*(1-TP[1])+(1-F[n,t])*TP[2];
-        		F[n,t+1] = p1/(p1+p2);
-        		pred[n,t+1] = F[n,t+1];
-			}
+            F[n,t] = F[n,t-1] * trans_mat[] * B[];
 		}
 	} 
 	//backwards algorithm
   	for (n in 1:N) { 
-		B1[n,T] = 1; 
-     	B2[n,T] = 1; 
+        B[n,T] = 1;
+		//B1[n,T] = 1; 
+     	//B2[n,T] = 1; 
   	}
   	for (t in 1:(T-1)){
-    	i = t*(-1)+10;      // transform t to get a backwards loop
+    	i = T - t;      // transform t to get a backwards loop
     	for (n in 1:N){
-      	like1 = exp(normal_lpdf(y[n,i+1]|mu[1],sigma[1]));
-      	like2 = exp(normal_lpdf(y[n,i+1]|mu[2],sigma[2]));  
-
-      	B1[n,i] = TP[1]*like1*B1[n,(i+1)]+(1-TP[2])*like2*B2[n,(i+1)];
-      	B2[n,i] = (1-TP[1])*like1*B1[n,(i+1)]+TP[2]*like2*B2[n,(i+1)];
-
-      	k = B1[n,i]+B2[n,i];
-      	B1[n,i] = B1[n,i]/k;
-      	B2[n,i] = B2[n,i]/k;
+            B[n,t] = trans_mat[] * emit_mat[] * B[n,t+1];
     	}
   	}
-  	// put it all together
-  	//for (t in 1:T){
-    	//for (n in 1:N) {
-      	//Z1[n,t] = F[n,t]*B1[n,t];
-      	//Z2[n,t] = (1-F[n,t])*B2[n,t];
-      	//prob_grp[n,t] = Z1[n,t]/(Z1[n,t]+Z2[n,t]);
-    	//}
-  	//}
-
-	}
 }
 model {
-	real ps; // temp for log component densities
-	sigma ~ cauchy(0,2.5);
-	mu ~ normal(0,10);
 
 	for (t in 1:T){
   		for (n in 1:N) {
